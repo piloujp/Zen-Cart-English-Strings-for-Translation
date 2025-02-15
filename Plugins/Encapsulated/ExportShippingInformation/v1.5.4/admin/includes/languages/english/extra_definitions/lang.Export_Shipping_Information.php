586f9911342f7f036b8c982b3e77970fc7c77f04
piloujp <117885587+piloujp@users.noreply.github.com>
2025-02-15 17:24:29 +0900

    Added Export Shipping Information plugin file

---
+++ Plugins/Encapsulated/ExportShippingInformation/v1.5.4/admin/includes/languages/english/extra_definitions/lang.Export_Shipping_Information.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright 2003-2025 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: piloujp 2025 Feb 15 Modified in v2.1.0 $
+*/
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_EXPORTSHIPPINGINFORMATION' => 'Export Shipping + Order Information II',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_EXPORTSHIPPINGINFORMATION' => 'Export your Zen Cart Order Shipping Information to CSV format for use in external programs.',
+];
+
+return $define;
