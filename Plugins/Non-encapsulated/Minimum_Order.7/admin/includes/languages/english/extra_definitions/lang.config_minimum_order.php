792abf8bc4a136b85e083aa3b1a63b3da9b835e0
piloujp <117885587+piloujp@users.noreply.github.com>
2025-02-22 16:57:23 +0900

    Added Minimum Order plugin file

---
+++ Plugins/Non-encapsulated/Minimum_Order.7/admin/includes/languages/english/extra_definitions/lang.config_minimum_order.php
@@ -0,0 +1,10 @@
+<?php
+
+$define = [
+    'CFGTITLE_MIN_ORDER_AMOUNT' => 'Minimum Order Amount',
+    'CFGDESC_MIN_ORDER_AMOUNT' => 'The minimum amount an order must total to be able to checkout. Empty means no minimum.',
+    'CFGTITLE_MIN_FIRST_ORDER_AMOUNT' => 'Minimum First Order Amount',
+    'CFGDESC_MIN_FIRST_ORDER_AMOUNT' => 'The minimum amount a first order must total to be able to checkout. Empty means no minimum. Make it the same as the Minimum Order Amount, if you always want the same minimum.',
+];
+
+return $define;
