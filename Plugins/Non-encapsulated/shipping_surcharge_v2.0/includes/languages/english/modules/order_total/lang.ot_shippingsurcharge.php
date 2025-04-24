2e25a7f1512b8a9298d4423757b2813384d6541f
piloujp <117885587+piloujp@users.noreply.github.com>
2025-04-24 15:57:47 +0900

    Added ShippingvSurcharge module file

---
+++ Plugins/Non-encapsulated/shipping_surcharge_v2.0/includes/languages/english/modules/order_total/lang.ot_shippingsurcharge.php
@@ -0,0 +1,22 @@
+<?php
+$define = [
+    'MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_TITLE' => 'Shipping Surcharge',
+    'MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_DESCRIPTION' => 'Shipping Surcharge',
+// Beginning of constant configuration titles and descriptions for order total module ot_shippingsurcharge
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_STATUS' => 'Charge a Shipping Surcharge',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_STATUS' => 'Do you want to have a shipping surcharge?',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_VALUE_THRESHOLD' => 'Order amount threshold for Shipping Surcharge',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_VALUE_THRESHOLD' => 'Charge shipping surcharge for orders amount over this value.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_WEIGHT_THRESHOLD' => 'Weight threshold for Shipping Surcharge',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_WEIGHT_THRESHOLD' => 'Charge shipping surcharge for orders over this weight.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_AMOUNT' => 'Surcharge Amount',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_AMOUNT' => 'For Percentage Calculation - include a % Example: 10%<br />For a flat amount just enter the amount - Example: 5 for $5.00',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_TAX_CLASS' => 'Tax Class',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_TAX_CLASS' => 'Use the following tax class on the shipping surcharge.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_ZONE' => 'Surcharge Zone',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_ZONE' => 'If a zone is selected, surcharge only for that zone.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_SORT_ORDER' => 'Sort Order',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPINGSURCHARGE_SORT_ORDER' => 'Sort order of display.',
+// eof constant configuration titles and descriptions for order total module ot_shippingsurcharge
+];
+return $define;
