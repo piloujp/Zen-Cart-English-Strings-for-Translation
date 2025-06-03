d77a536c6f1ca76a30f828feddf501d67b4d75f5
piloujp <117885587+piloujp@users.noreply.github.com>
2025-06-04 00:43:08 +0900

    Plugin Inventory Updator file added

---
+++ Plugins/Encapsulated/InvUpdator/1.2.6/admin/includes/languages/english/extra_definitions/lang.invupdator.php
@@ -0,0 +1,8 @@
+<?php
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_INVUPDATOR' => 'Inventory Updator',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_INVUPDATOR' => 'Update Inventory and Prices all in 1 Place',
+];
+
+return $define;
