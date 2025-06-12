876492a7c00d0ce791af36c7ba747df19c03b4c0
piloujp <117885587+piloujp@users.noreply.github.com>
2025-06-12 15:39:21 +0900

    Added offsite payment file

---
+++ Plugins/Non-encapsulated/offsitepayment-v3/includes/languages/english/modules/payment/lang.offsitepayment.php
@@ -0,0 +1,18 @@
+<?php
+
+$define = [
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_TITLE' => 'Bill Me / Pay Later',
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_DESCRIPTION' => '',
+    'MODULE_PAYMENT_OFFSITEPAYMENT_TEXT_EMAIL_FOOTER' => '', // 'We will email you a payment request for the order total.'
+// Beginning of constant configuration titles and descriptions for payment module offsitepayment
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_STATUS' => 'Enable Offsite Payment Module',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_STATUS' => 'Do you want to accept Offsite payments?',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_SORT_ORDER' => 'Sort order of display.',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_SORT_ORDER' => 'Sort order of display. Lowest is displayed first.',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_ZONE' => 'Payment Zone',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_ZONE' => 'If a zone is selected, only enable this payment method for that zone.',
+    'CFGTITLE_MODULE_PAYMENT_OFFSITEPAYMENT_ORDER_STATUS_ID' => 'Set Order Status',
+    'CFGDESC_MODULE_PAYMENT_OFFSITEPAYMENT_ORDER_STATUS_ID' => 'Set the status of orders made with this payment module to this value',
+];
+
+return $define;
