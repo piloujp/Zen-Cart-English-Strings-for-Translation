462c1847c5fd60f36bae2fffc96ab756bfbc7362
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 05:11:34 +0900

    New translations lang.ot_total.php (French)

---
+++ admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_total.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'MODULE_ORDER_TOTAL_TOTAL_TITLE' => 'Total',
+    'MODULE_ORDER_TOTAL_TOTAL_DESCRIPTION' => 'Total de la commande',
+// Beginning of constant configuration titles and descriptions for order total module ot_total
+    'CFGTITLE_MODULE_ORDER_TOTAL_TOTAL_STATUS' => 'Ce module est installé',
+    'CFGDESC_MODULE_ORDER_TOTAL_TOTAL_STATUS' => '',
+    'CFGTITLE_MODULE_ORDER_TOTAL_TOTAL_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_ORDER_TOTAL_TOTAL_SORT_ORDER' => 'Ordre d\'affichage.',
+// End of constant configuration titles and descriptions for order total module ot_total
+];
+
+return $define;
