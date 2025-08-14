f744c1d22467c69efb4e2157c8d5806ae5e8fa26
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 02:22:54 +0900

    New translations lang.ot_subtotal.php (French)

--- admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_subtotal.php
+++ admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_subtotal.php
@@ -1,12 +1,12 @@
 <?php
 $define = [
-    'MODULE_ORDER_TOTAL_SUBTOTAL_TITLE' => 'Sub-Total',
-    'MODULE_ORDER_TOTAL_SUBTOTAL_DESCRIPTION' => 'Order Sub-Total',
+    'MODULE_ORDER_TOTAL_SUBTOTAL_TITLE' => 'Sous-Total',
+    'MODULE_ORDER_TOTAL_SUBTOTAL_DESCRIPTION' => 'Sous-Total Commande',
 // Beginning of constant configuration titles and descriptions for order total module ot_subtotal
-    'CFGTITLE_MODULE_ORDER_TOTAL_SUBTOTAL_STATUS' => 'This module is installed',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SUBTOTAL_STATUS' => 'Ce module est installé',
     'CFGDESC_MODULE_ORDER_TOTAL_SUBTOTAL_STATUS' => '',
-    'CFGTITLE_MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER' => 'Sort Order',
-    'CFGDESC_MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER' => 'Sort order of display.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER' => 'Ordre d\'affichage.',
 // End of constant configuration titles and descriptions for order total module ot_subtotal
 ];
 
