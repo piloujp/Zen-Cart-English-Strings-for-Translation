421c3388f03090a3e84430fd0fd01ee4342fb5df
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 00:57:35 +0900

    New translations lang.freeshipper.php (Japanese)

---
+++ admin_languages_packs/Japanese/ZenCart_folder/includes/languages/english/modules/shipping/lang.freeshipper.php
@@ -0,0 +1,22 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_FREESHIPPER_TEXT_TITLE' => 'FREE SHIPPING!',
+    'MODULE_SHIPPING_FREESHIPPER_TEXT_DESCRIPTION' => 'FREE SHIPPING',
+    'MODULE_SHIPPING_FREESHIPPER_TEXT_WAY' => 'No Delivery Charge',
+// Beginning of constant configuration titles and descriptions for freeshipper Shipping
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_STATUS' => 'Enable Free Shipping',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_STATUS' => 'Do you want to offer Free shipping?',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_COST' => 'Free Shipping Cost',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_COST' => 'What is the Shipping cost?',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_HANDLING' => 'Handling Fee',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_HANDLING' => 'Handling fee for this shipping method.',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_TAX_CLASS' => 'Tax Class',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_TAX_CLASS' => 'Use the following tax class on the shipping fee.',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_ZONE' => 'Shipping Zone',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_ZONE' => 'If a zone is selected, only enable this shipping method for that zone.',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_SORT_ORDER' => 'Sort Order',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_SORT_ORDER' => 'Sort order of display.',
+// End of constant configuration titles and descriptions for freeshipper Shipping
+];
+
+return $define;
