eba754aef50167e4785829effe98035d99ade7aa
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 02:22:52 +0900

    New translations lang.ot_shipping.php (French)

--- admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_shipping.php
+++ admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_shipping.php
@@ -1,21 +1,21 @@
 <?php
 // FIXME - move the last two strings into a shared file in next update
 $define = [
-    'MODULE_ORDER_TOTAL_SHIPPING_TITLE' => 'Shipping',
-    'MODULE_ORDER_TOTAL_SHIPPING_DESCRIPTION' => 'Order Shipping Cost',
-    'FREE_SHIPPING_TITLE' => 'Free Shipping',
-    'FREE_SHIPPING_DESCRIPTION' => 'Free shipping for orders over %s',
+    'MODULE_ORDER_TOTAL_SHIPPING_TITLE' => 'Frais de Port',
+    'MODULE_ORDER_TOTAL_SHIPPING_DESCRIPTION' => 'Frais de port pour la commande',
+    'FREE_SHIPPING_TITLE' => 'Livraison Gratuite',
+    'FREE_SHIPPING_DESCRIPTION' => 'Livraison gratuite pour les commandes d\'un montant supérieur à %s',
 // Beginning of constant configuration titles and descriptions for order total module ot_shipping
-    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_STATUS' => 'This module is installed',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_STATUS' => 'Ce module est installé',
     'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_STATUS' => '',
-    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_SORT_ORDER' => 'Sort Order',
-    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_SORT_ORDER' => 'Sort order of display.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_SORT_ORDER' => 'Ordre d\'affichage.',
     'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING' => 'Allow Free Shipping',
     'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING' => 'Do you want to allow free shipping?',
     'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER' => 'Free Shipping For Orders Over',
     'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER' => 'Provide free shipping for orders over the set amount.',
-    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_DESTINATION' => 'Provide Free Shipping For Orders Made',
-    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_DESTINATION' => 'Provide free shipping for orders sent to the set destination.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_DESTINATION' => 'Offrir la livraison gratuite pour les commandes',
+    'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_DESTINATION' => 'Offrir la livraison gratuite pour les commandes envoyées aux destinations :',
 // End of constant configuration titles and descriptions for order total module ot_shipping
 ];
 
