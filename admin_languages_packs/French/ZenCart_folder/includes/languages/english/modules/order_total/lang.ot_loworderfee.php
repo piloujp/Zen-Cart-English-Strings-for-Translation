3e0f7116390012e62da4ae643d87c17e718a3812
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 02:22:50 +0900

    New translations lang.ot_loworderfee.php (French)

--- admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_loworderfee.php
+++ admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_loworderfee.php
@@ -1,26 +1,26 @@
 <?php
 $define = [
-    'MODULE_ORDER_TOTAL_LOWORDERFEE_TITLE' => 'Low Order Fee',
-    'MODULE_ORDER_TOTAL_LOWORDERFEE_DESCRIPTION' => 'Low Order Fee',
+    'MODULE_ORDER_TOTAL_LOWORDERFEE_TITLE' => 'Frais pour les petites commandes',
+    'MODULE_ORDER_TOTAL_LOWORDERFEE_DESCRIPTION' => 'Frais pour les commandes à montant total bas',
 // Beginning of constant configuration titles and descriptions for order total module ot_loworderfee
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_STATUS' => 'This module is installed',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_STATUS' => 'Ce module est installé',
     'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_STATUS' => '',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_SORT_ORDER' => 'Sort Order',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_SORT_ORDER' => 'Sort order of display.',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_LOW_ORDER_FEE' => 'Allow Low Order Fee',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_LOW_ORDER_FEE' => 'Do you want to allow low order fees?',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_ORDER_UNDER' => 'Order Fee For Orders Under',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_ORDER_UNDER' => 'Add the low order fee to orders under this amount.',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_FEE' => 'Order Fee',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_FEE' => 'For Percentage Calculation - include a % Example: 10%<br>For a flat amount just enter the amount - Example: 5 for $5.00',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_DESTINATION' => 'Attach Low Order Fee On Orders Made',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_DESTINATION' => 'Attach low order fee for orders sent to the set destination.',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_TAX_CLASS' => 'Tax Class',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_TAX_CLASS' => 'Use the following tax class on the low order fee.',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_VIRTUAL' => 'No Low Order Fee on Virtual Products',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_VIRTUAL' => 'Do not charge Low Order Fee when cart is Virtual Products Only',
-    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_GV' => 'No Low Order Fee on Gift Vouchers',
-    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_GV' => 'Do not charge Low Order Fee when cart is Gift Vouchers Only',// End of constant configuration titles and descriptions for order total module ot_loworderfee
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_SORT_ORDER' => 'Ordre d\'affichage.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_LOW_ORDER_FEE' => 'Autoriser des frais pour les petites commandes',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_LOW_ORDER_FEE' => 'Souhaitez-vous autoriser des frais de commande supplémentaires pour les petites commandes ?',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_ORDER_UNDER' => 'Frais de commande pour les commandes inférieures à',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_ORDER_UNDER' => 'Ajoutez les frais de petites commandes aux commandes inférieures à ce montant.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_FEE' => 'Frais de commande',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_FEE' => 'Pour le calcul du pourcentage, incluez un %. Exemple : 10 %<br />Pour un montant fixe, saisissez simplement le montant - Exemple : 5 pour €5.00.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_DESTINATION' => 'Ajouter les frais de petites commandes aux commandes',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_DESTINATION' => 'Appliquez des frais de petites commandes pour les commandes envoyées aux destinations :',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_TAX_CLASS' => 'Classe de la taxe',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_TAX_CLASS' => 'Utilisez la classe de taxe suivante.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_VIRTUAL' => 'Pas de frais supplémentaires sur les produits virtuels',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_VIRTUAL' => 'Ne facturez pas de frais de petites commandes lorsque le panier contient uniquement des produits virtuels.',
+    'CFGTITLE_MODULE_ORDER_TOTAL_LOWORDERFEE_GV' => 'Pas de frais supplémentaires sur les chèques-cadeaux',
+    'CFGDESC_MODULE_ORDER_TOTAL_LOWORDERFEE_GV' => 'Ne facturez pas de frais de petites commandes lorsque le panier contient uniquement des chèques-cadeaux',// End of constant configuration titles and descriptions for order total module ot_loworderfee
 ];
 
 return $define;
