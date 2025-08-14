d2bf52998491fd1779de581f6a5c924533ede26d
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 02:23:15 +0900

    New translations lang.flat.php (French)

--- admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/shipping/lang.flat.php
+++ admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/shipping/lang.flat.php
@@ -1,21 +1,21 @@
 <?php
 $define = [
-    'MODULE_SHIPPING_FLAT_TEXT_TITLE' => 'Flat Rate',
-    'MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION' => 'Flat Rate',
-    'MODULE_SHIPPING_FLAT_TEXT_WAY' => 'Best Way',
+    'MODULE_SHIPPING_FLAT_TEXT_TITLE' => 'Au Forfait',
+    'MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION' => 'Forfait de livraison',
+    'MODULE_SHIPPING_FLAT_TEXT_WAY' => 'La meilleure façon',
 // Beginning of constant configuration titles and descriptions for flat Shipping
-    'CFGTITLE_MODULE_SHIPPING_FLAT_STATUS' => 'Enable Flat Shipping',
-    'CFGDESC_MODULE_SHIPPING_FLAT_STATUS' => 'Do you want to offer flat rate shipping?',
-    'CFGTITLE_MODULE_SHIPPING_FLAT_COST' => 'Shipping Cost',
-    'CFGDESC_MODULE_SHIPPING_FLAT_COST' => 'The shipping cost for all orders using this shipping method.',
-    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Tax Class',
-    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Use the following tax class on the shipping fee.',
-    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'Tax Basis',
-    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'On what basis is Shipping Tax calculated. Options are<br>Shipping - Based on customers Shipping Address<br>Billing Based on customers Billing address<br>Store - Based on Store address if Billing/Shipping Zone equals Store zone',
-    'CFGTITLE_MODULE_SHIPPING_FLAT_ZONE' => 'Shipping Zone',
-    'CFGDESC_MODULE_SHIPPING_FLAT_ZONE' => 'If a zone is selected, only enable this shipping method for that zone.',
-    'CFGTITLE_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Sort Order',
-    'CFGDESC_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Sort order of display.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_STATUS' => 'Activer l\'expédition forfaitaire',
+    'CFGDESC_MODULE_SHIPPING_FLAT_STATUS' => 'Souhaitez-vous proposer des frais de port à tarif forfaitaire ?',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_COST' => 'Frais de livraison',
+    'CFGDESC_MODULE_SHIPPING_FLAT_COST' => 'Les frais d\'expédition pour toutes les commandes qui utilisent ce mode d\'expédition.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Classe de la taxe',
+    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Utilisez la classe de taxe suivante sur les frais d’expédition.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'Base de calcul de la taxe',
+    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'Sur quelle base la taxe d\'expédition est-elle calculée ? Les options sont :<br>Expédition - Basée sur l\'adresse d\'expédition du client<br>Facturation - Basée sur l\'adresse de facturation du client<br>Magasin - Basée sur l\'adresse du magasin si la zone de facturation/d\'expédition est égale à la zone du magasin',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_ZONE' => 'Zone d\'expédition',
+    'CFGDESC_MODULE_SHIPPING_FLAT_ZONE' => 'Si une zone est sélectionnée, activez ce mode d\'expédition uniquement pour cette zone.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Ordre d\'affichage.',
 // End of constant configuration titles and descriptions for flat Shipping
 ];
 
