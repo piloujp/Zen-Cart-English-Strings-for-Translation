af6d2f8887516814ec945c1987971f538c320f26
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 02:23:25 +0900

    New translations lang.display_logs.php (French)

--- admin_languages_packs/French/ZenCart_folder/zc_plugins/DisplayLogs/v3.0.2/admin/includes/languages/english/extra_definitions/lang.display_logs.php
+++ admin_languages_packs/French/ZenCart_folder/zc_plugins/DisplayLogs/v3.0.2/admin/includes/languages/english/extra_definitions/lang.display_logs.php
@@ -1,16 +1,16 @@
 <?php
 $define = [
-    'ADMIN_PLUGIN_MANAGER_NAME_FOR_DISPLAYLOGS' => 'Display Logs',
-    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_DISPLAYLOGS' => 'Display and manage Zen Cart log files.',
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_DISPLAYLOGS' => 'Visualisation des fichiers journaux',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_DISPLAYLOGS' => 'Affiche et gère les fichiers journaux de Zen Cart.',
 // Display Logs - configuration
-    'CFGTITLE_DISPLAY_LOGS_MAX_DISPLAY' => 'Display Logs: Display Maximum',
-    'CFGDESC_DISPLAY_LOGS_MAX_DISPLAY' => 'Identify the maximum number of logs to display.  (Default: <b>20</b>)',
-    'CFGTITLE_DISPLAY_LOGS_MAX_FILE_SIZE' => 'Display Logs: Maximum File Size',
-    'CFGDESC_DISPLAY_LOGS_MAX_FILE_SIZE' => 'Identify the maximum size of any file to display.  (Default: <b>80000</b>)',
-    'CFGTITLE_DISPLAY_LOGS_INCLUDED_FILES' => 'Display Logs: Included File Prefixes',
-    'CFGDESC_DISPLAY_LOGS_INCLUDED_FILES' => 'Identify the log-file <em>prefixes</em> to include in the display, separated by the pipe character (|).  Any intervening spaces are removed by the processing code.',
-    'CFGTITLE_DISPLAY_LOGS_EXCLUDED_FILES' => 'Display Logs: Excluded File Prefixes',
-    'CFGDESC_DISPLAY_LOGS_EXCLUDED_FILES' => 'Identify the log-file prefixes to <em>exclude</em> from the display, separated by the pipe character (|). Any intervening spaces are removed by the processing code.',
+    'CFGTITLE_DISPLAY_LOGS_MAX_DISPLAY' => 'Visualisation des fichiers journaux : Affichage Maximum',
+    'CFGDESC_DISPLAY_LOGS_MAX_DISPLAY' => 'Défini le nombre maximale de fichiers journaux à afficher.  (Par défaut : <b>20</b>)',
+    'CFGTITLE_DISPLAY_LOGS_MAX_FILE_SIZE' => 'Visualisation des fichiers journaux : Taille maximale des fichiers',
+    'CFGDESC_DISPLAY_LOGS_MAX_FILE_SIZE' => 'Identifie la taille maximale de tout fichier à afficher. (Par défaut : <b>80000</b>)',
+    'CFGTITLE_DISPLAY_LOGS_INCLUDED_FILES' => 'Visualisation des fichiers journaux : Préfixes de fichiers inclus',
+    'CFGDESC_DISPLAY_LOGS_INCLUDED_FILES' => 'Identifiez les préfixes du fichier journal à inclure dans l\'affichage, séparés par le caractère pipe (|). Tous les espaces intermédiaires sont supprimés par le code de traitement.',
+    'CFGTITLE_DISPLAY_LOGS_EXCLUDED_FILES' => 'Visualisation des fichiers journaux : Préfixes de fichiers exclus',
+    'CFGDESC_DISPLAY_LOGS_EXCLUDED_FILES' => 'Identifiez les préfixes du fichier journal à <em>exclure</em> de l\'affichage, séparés par le caractère pipe (|). Tous les espaces intermédiaires sont supprimés par le code de traitement.',
 ];
 
 return $define;
