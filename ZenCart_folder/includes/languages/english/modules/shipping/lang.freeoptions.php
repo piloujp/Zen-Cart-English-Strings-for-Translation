4e953589b759df1b1590b77dcf49d94715e0138d
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 00:22:59 +0900

    Corrected messed up POSM files

--- ZenCart_folder/includes/languages/english/modules/shipping/lang.freeoptions.php
+++ ZenCart_folder/includes/languages/english/modules/shipping/lang.freeoptions.php
@@ -8,8 +8,7 @@ The Free Options module does not show when Free Shipper is displayed.<br><br>
 Setting Total to >= 0.00 and <= nothing (leave blank) will activate this module to show with all shipping modules, except for Free Shipping - freeshipper.<br><br>
 NOTE: Leaving all settings for Total, Weight and Item count blank will deactivate this module.<br><br>
 NOTE: Free Shipping Options does not display if Free Shipping is used based on 0 weight is Free Shipping.
-See: freeshipper
-',
+See: freeshipper',
     'MODULE_SHIPPING_FREEOPTIONS_TEXT_WAY' => 'Free Shipping',
 // Beginning of constant configuration titles and descriptions for freeoptions Shipping
     'CFGTITLE_MODULE_SHIPPING_FREEOPTIONS_STATUS' => 'Enable Free Options Shipping',
