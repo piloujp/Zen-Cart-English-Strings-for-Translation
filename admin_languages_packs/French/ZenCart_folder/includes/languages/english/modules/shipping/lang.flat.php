0c2b6084d7ec110d95175139b465d29e15803249
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 00:57:27 +0900

    New translations lang.flat.php (French)

---
+++ admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/shipping/lang.flat.php
@@ -0,0 +1,22 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_FLAT_TEXT_TITLE' => 'Flat Rate',
+    'MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION' => 'Flat Rate',
+    'MODULE_SHIPPING_FLAT_TEXT_WAY' => 'Best Way',
+// Beginning of constant configuration titles and descriptions for flat Shipping
+    'CFGTITLE_MODULE_SHIPPING_FLAT_STATUS' => 'Enable Flat Shipping',
+    'CFGDESC_MODULE_SHIPPING_FLAT_STATUS' => 'Do you want to offer flat rate shipping?',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_COST' => 'Shipping Cost',
+    'CFGDESC_MODULE_SHIPPING_FLAT_COST' => 'The shipping cost for all orders using this shipping method.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Tax Class',
+    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Use the following tax class on the shipping fee.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'Tax Basis',
+    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'On what basis is Shipping Tax calculated. Options are<br>Shipping - Based on customers Shipping Address<br>Billing Based on customers Billing address<br>Store - Based on Store address if Billing/Shipping Zone equals Store zone',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_ZONE' => 'Shipping Zone',
+    'CFGDESC_MODULE_SHIPPING_FLAT_ZONE' => 'If a zone is selected, only enable this shipping method for that zone.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Sort Order',
+    'CFGDESC_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Sort order of display.',
+// End of constant configuration titles and descriptions for flat Shipping
+];
+
+return $define;
