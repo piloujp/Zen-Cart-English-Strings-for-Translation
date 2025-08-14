a43458673fb090d6abe030c66578d753e68e7a93
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-15 00:56:56 +0900

    New translations lang.ot_tax.php (Spanish)

---
+++ admin_languages_packs/Spanish/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_tax.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'MODULE_ORDER_TOTAL_TAX_TITLE' => 'Tax',
+    'MODULE_ORDER_TOTAL_TAX_DESCRIPTION' => 'Order Tax',
+// Beginning of constant configuration titles and descriptions for order total module ot_tax
+    'CFGTITLE_MODULE_ORDER_TOTAL_TAX_STATUS' => 'This module is installed',
+    'CFGDESC_MODULE_ORDER_TOTAL_TAX_STATUS' => '',
+    'CFGTITLE_MODULE_ORDER_TOTAL_TAX_SORT_ORDER' => 'Sort Order',
+    'CFGDESC_MODULE_ORDER_TOTAL_TAX_SORT_ORDER' => 'Sort order of display.',
+// End of constant configuration titles and descriptions for order total module ot_tax
+];
+
+return $define;
