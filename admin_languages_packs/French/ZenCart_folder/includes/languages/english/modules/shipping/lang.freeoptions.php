7ca1878cf99b7e7278b4ab587799d4fda4a23a74
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-16 18:17:21 +0900

    New translations lang.freeoptions.php (French)

--- admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/shipping/lang.freeoptions.php
+++ admin_languages_packs/French/ZenCart_folder/includes/languages/english/modules/shipping/lang.freeoptions.php
@@ -5,7 +5,7 @@ $define = [
 freeoptions est utilisé pour afficher une option de livraison gratuite lorsque d\'autres modules de livraison sont affichés.
 Il peut être basé sur: toujours afficher, Total commande, Poids commande, Nombre d\'articles commandés.
 Le module freeoptions ne s\'affiche pas quand freeshipper est actif.<br /><br />
-Mettre un total à &gt;= 0.00 et &lt;= rien (en blanc) activera ce module pour qu\'il s\'affiche avec les autres modules, sauf pour LIVRAISON GRATUITE - freeshipper.<br /><br />
+Mettre un total à &gt;= 0,00 et &lt;= rien (vide) activera ce module pour qu\'il s\'affiche avec les autres modules, sauf pour LIVRAISON GRATUITE - freeshipper.<br /><br />
 NOTE: le fait de laisser tous les réglages vides pour Total, Poids et Nombre d\'articles désactivera ce module.<br /><br />
 NOTE: les options de livraison gratuite ne s\'affichent pas si LIVRAISON GRATUITE est utilisé avec un poids de 0.
 Voir: freeshipper
