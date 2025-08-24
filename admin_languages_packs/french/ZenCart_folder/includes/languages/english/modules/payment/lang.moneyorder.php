c9ba1d730cff1c3a2bf275aaecb227c3dec0ab4b
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:00:31 +0900

    New translations lang.moneyorder.php (french)

--- admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/payment/lang.moneyorder.php
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/payment/lang.moneyorder.php
@@ -2,7 +2,7 @@
 $define = [
     'MODULE_PAYMENT_MONEYORDER_TEXT_TITLE' => 'Chèque/Mandat postal',
     'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Les clients peuvent envoyer leur paiement par courrier. Leur e-mail de confirmation de commande leur demandera de : <br><br>Veuillez établir votre chèque ou virement bancaire à l\'ordre de :<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? MODULE_PAYMENT_MONEYORDER_PAYTO : '<br>(le nom de votre magasin)') . '<br><br>Envoyez votre paiement à :<br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'Votre commande ne sera pas expédiée tant que nous n\'aurons pas reçu le paiement.',
-// Beginning of constant configuration titles and descriptions for payment module moneyorder
+// bof constant configuration titles and descriptions for payment module moneyorder
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_STATUS' => 'Activer le module « Chèque/Mandat postal »',
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_STATUS' => 'Souhaitez-vous accepter les paiements par chèque/mandat postal ?',
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_PAYTO' => 'À l\'ordre de :',
@@ -13,7 +13,7 @@ $define = [
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_ZONE' => 'Si une zone est sélectionnée, activez uniquement ce mode de paiement pour cette zone.',
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_ORDER_STATUS_ID' => 'Définir le statut de la commande',
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_ORDER_STATUS_ID' => 'Définir le statut des commandes effectuées avec ce module de paiement sur cette valeur :',
-// End of constant configuration titles and descriptions for payment module moneyorder
+// eof constant configuration titles and descriptions for payment module moneyorder
 ];
 if (defined('MODULE_PAYMENT_MONEYORDER_STATUS')) {
     $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = 'Veuillez établir votre chèque ou virement bancaire à l\'ordre de :' . "\n\n" . MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\n" . 'Envoyez votre paiement à :' . "\n" . STORE_NAME_ADDRESS . "\n\n" . 'Votre commande ne sera envoyée qu\'à réception du règlement et de sa validation par notre banque.';
