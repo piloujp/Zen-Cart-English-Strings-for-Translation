8dda41c2172dc66c774414a6549ea9cb39215bbe
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:00:19 +0900

    New translations lang.ot_total.php (french)

--- admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_total.php
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_total.php
@@ -2,12 +2,12 @@
 $define = [
     'MODULE_ORDER_TOTAL_TOTAL_TITLE' => 'Total',
     'MODULE_ORDER_TOTAL_TOTAL_DESCRIPTION' => 'Total de la commande',
-// Beginning of constant configuration titles and descriptions for order total module ot_total
+// bof constant configuration titles and descriptions for order total module ot_total
     'CFGTITLE_MODULE_ORDER_TOTAL_TOTAL_STATUS' => 'Ce module est installé',
     'CFGDESC_MODULE_ORDER_TOTAL_TOTAL_STATUS' => '',
     'CFGTITLE_MODULE_ORDER_TOTAL_TOTAL_SORT_ORDER' => 'Ordre de tri',
     'CFGDESC_MODULE_ORDER_TOTAL_TOTAL_SORT_ORDER' => 'Ordre d\'affichage.',
-// End of constant configuration titles and descriptions for order total module ot_total
+// eof constant configuration titles and descriptions for order total module ot_total
 ];
 
 return $define;
