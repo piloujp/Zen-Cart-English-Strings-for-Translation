65919ae89e6f5c022a0eac9101068c2a77cafb1d
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:00:28 +0900

    New translations lang.cod.php (japanese)

--- admin_languages_packs/japanese/ZenCart_folder/includes/languages/english/modules/payment/lang.cod.php
+++ admin_languages_packs/japanese/ZenCart_folder/includes/languages/english/modules/payment/lang.cod.php
@@ -1,9 +1,8 @@
 <?php
-
 $define = [
     'MODULE_PAYMENT_COD_TEXT_TITLE' => '代金引換',
     'MODULE_PAYMENT_COD_TEXT_DESCRIPTION' => '代金引換',
-// Beginning of constant configuration titles and descriptions for payment module cod
+// bof constant configuration titles and descriptions for payment module cod
     'CFGTITLE_MODULE_PAYMENT_COD_STATUS' => '代金引換を有効にする',
     'CFGDESC_MODULE_PAYMENT_COD_STATUS' => '代金引換を受け取りますか？',
     'CFGTITLE_MODULE_PAYMENT_COD_ZONE' => '支払い地帯',
@@ -12,7 +11,7 @@ $define = [
     'CFGDESC_MODULE_PAYMENT_COD_SORT_ORDER' => '表示順を設定します。 最下位が最初に表示されます。',
     'CFGTITLE_MODULE_PAYMENT_COD_ORDER_STATUS_ID' => '注文ステータスの設定',
     'CFGDESC_MODULE_PAYMENT_COD_ORDER_STATUS_ID' => 'この支払いモジュールで行われた注文のステータスを設定します。',
-// End of constant configuration titles and descriptions for payment module cod
+// eof constant configuration titles and descriptions for payment module cod
 ];
 
 return $define;
