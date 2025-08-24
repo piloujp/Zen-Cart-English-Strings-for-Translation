88106416a14ed860b0a7f91a5c3ada26696a7395
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:01:01 +0900

    New translations lang.system_inspection.php (french)

--- admin_languages_packs/french/ZenCart_folder/zc_plugins/SystemInspection/1.6.0/admin/includes/languages/english/extra_definitions/lang.system_Inspection.php
+++ admin_languages_packs/french/ZenCart_folder/zc_plugins/SystemInspection/1.6.0/admin/includes/languages/english/extra_definitions/lang.system_Inspection.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * @copyright Copyright 2003-2024 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: piloujp 2025 Feb 12 Modified in v2.1.0 $
-*/
-
 $define = [
     'ADMIN_PLUGIN_MANAGER_NAME_FOR_SYSTEMINSPECTION' => 'Inspection du système',
     'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SYSTEMINSPECTION' => 'Répertorie les modifications de la base de données qui ont été apportées à une installation de base de Zen Cart (anciennement appelé ModList).',
