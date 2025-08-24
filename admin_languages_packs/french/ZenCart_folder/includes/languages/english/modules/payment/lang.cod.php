533c4ce88ae395239f0d7248a17e0b449339ef6b
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 23:00:01 +0900

    New translations lang.cod.php (french)

---
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/payment/lang.cod.php
@@ -0,0 +1,17 @@
+<?php
+$define = [
+    'MODULE_PAYMENT_COD_TEXT_TITLE' => 'Contre-Remboursement',
+    'MODULE_PAYMENT_COD_TEXT_DESCRIPTION' => 'Contre-Remboursement',
+// bof constant configuration titles and descriptions for payment module cod
+    'CFGTITLE_MODULE_PAYMENT_COD_STATUS' => 'Proposer le paiement à la livraison (contre remboursement)',
+    'CFGDESC_MODULE_PAYMENT_COD_STATUS' => 'Voulez-vous que ce module s\'affiche ?',
+    'CFGTITLE_MODULE_PAYMENT_COD_ZONE' => 'Zone de paiement',
+    'CFGDESC_MODULE_PAYMENT_COD_ZONE' => 'Si une zone est sélectionnée, activez uniquement ce mode de paiement pour cette zone.',
+    'CFGTITLE_MODULE_PAYMENT_COD_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_PAYMENT_COD_SORT_ORDER' => 'Ordre d\'affichage.',
+    'CFGTITLE_MODULE_PAYMENT_COD_ORDER_STATUS_ID' => 'Définir le statut de la commande',
+    'CFGDESC_MODULE_PAYMENT_COD_ORDER_STATUS_ID' => 'Définir le statut des commandes effectuées avec ce module de paiement sur cette valeur :',
+// eof constant configuration titles and descriptions for payment module cod
+];
+
+return $define;
