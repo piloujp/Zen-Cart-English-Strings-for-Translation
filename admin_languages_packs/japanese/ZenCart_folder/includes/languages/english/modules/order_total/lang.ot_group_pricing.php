63962af5c4ebb6ecc1fc80eba29fab5e9afb3af7
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 19:06:31 +0900

    New translations lang.ot_group_pricing.php (japanese)

---
+++ admin_languages_packs/japanese/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_group_pricing.php
@@ -0,0 +1,21 @@
+<?php
+$define = [
+    'MODULE_ORDER_TOTAL_GROUP_PRICING_TITLE' => '顧客割引',
+    'MODULE_ORDER_TOTAL_GROUP_PRICING_DESCRIPTION' => '顧客割引',
+// Beginning of constant configuration titles and descriptions for order total module ot_group_pricing
+    'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_STATUS' => 'このモジュールはインストールされています',
+    'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_STATUS' => '',
+    'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_SORT_ORDER' => '表示順',
+    'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_SORT_ORDER' => '表示順を設定します。 最下位が最初に表示されます。',
+    'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_INC_SHIPPING' => '送料込み',
+    'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_INC_SHIPPING' => '割引計算の前に配送料を金額に含めますか？',
+    'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_INC_TAX' => '税込',
+    'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_INC_TAX' => '割引計算前の金額に税金を含めますか？',
+    'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_CALC_TAX' => '税金を再計算',
+    'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_CALC_TAX' => '税金の再計算方法。',
+    'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_TAX_CLASS' => '税区分',
+    'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_TAX_CLASS' => 'グループ割引をクレジット ノートとして扱う場合は、次の税金クラスを使用します。',
+// End of constant configuration titles and descriptions for order total module ot_group_pricing
+];
+
+return $define;
