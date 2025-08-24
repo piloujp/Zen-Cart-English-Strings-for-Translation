ae56561c3ee775cdb9f995e77bd90fed5d383913
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 23:00:36 +0900

    New translations lang.display_logs.php (french)

---
+++ admin_languages_packs/french/ZenCart_folder/zc_plugins/DisplayLogs/v3.0.3/admin/includes/languages/english/extra_definitions/lang.display_logs.php
@@ -0,0 +1,16 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_DISPLAYLOGS' => 'Visualisation des fichiers journaux',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_DISPLAYLOGS' => 'Affiche et gère les fichiers journaux de Zen Cart.',
+// Display Logs - configuration
+    'CFGTITLE_DISPLAY_LOGS_MAX_DISPLAY' => 'Visualisation des fichiers journaux : Affichage Maximum',
+    'CFGDESC_DISPLAY_LOGS_MAX_DISPLAY' => 'Défini le nombre maximale de fichiers journaux à afficher.  (Par défaut : <b>20</b>)',
+    'CFGTITLE_DISPLAY_LOGS_MAX_FILE_SIZE' => 'Visualisation des fichiers journaux : Taille maximale des fichiers',
+    'CFGDESC_DISPLAY_LOGS_MAX_FILE_SIZE' => 'Identifie la taille maximale de tout fichier à afficher. (Par défaut : <b>80000</b>)',
+    'CFGTITLE_DISPLAY_LOGS_INCLUDED_FILES' => 'Visualisation des fichiers journaux : Préfixes de fichiers inclus',
+    'CFGDESC_DISPLAY_LOGS_INCLUDED_FILES' => 'Identifiez les préfixes du fichier journal à inclure dans l\'affichage, séparés par le caractère pipe (|). Tous les espaces intermédiaires sont supprimés par le code de traitement.',
+    'CFGTITLE_DISPLAY_LOGS_EXCLUDED_FILES' => 'Visualisation des fichiers journaux : Préfixes de fichiers exclus',
+    'CFGDESC_DISPLAY_LOGS_EXCLUDED_FILES' => 'Identifiez les préfixes du fichier journal à <em>exclure</em> de l\'affichage, séparés par le caractère pipe (|). Tous les espaces intermédiaires sont supprimés par le code de traitement.',
+];
+
+return $define;
