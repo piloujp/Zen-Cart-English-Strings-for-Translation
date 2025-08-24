a3f9eeffb7ec3300ab1353569a31c51aedcb8f47
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:00:04 +0900

    New translations lang.ot_group_pricing.php (french)

--- admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_group_pricing.php
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_group_pricing.php
@@ -2,7 +2,7 @@
 $define = [
     'MODULE_ORDER_TOTAL_GROUP_PRICING_TITLE' => 'Réduction de Groupe',
     'MODULE_ORDER_TOTAL_GROUP_PRICING_DESCRIPTION' => 'Réduction de Groupe',
-// Beginning of constant configuration titles and descriptions for order total module ot_group_pricing
+// bof constant configuration titles and descriptions for order total module ot_group_pricing
     'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_STATUS' => 'Ce module est installé',
     'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_STATUS' => '',
     'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_SORT_ORDER' => 'Ordre de tri',
@@ -15,7 +15,7 @@ $define = [
     'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_CALC_TAX' => 'Méthode de recalcule de la taxe.',
     'CFGTITLE_MODULE_ORDER_TOTAL_GROUP_PRICING_TAX_CLASS' => 'Classe de la taxe',
     'CFGDESC_MODULE_ORDER_TOTAL_GROUP_PRICING_TAX_CLASS' => 'Utilisez la classe de taxe suivante quand la méthode de recalcule « Credit Note » est utilisée.',
-// End of constant configuration titles and descriptions for order total module ot_group_pricing
+// eof constant configuration titles and descriptions for order total module ot_group_pricing
 ];
 
 return $define;
