389e97ca9ba894852f97c4fe038ba239a83f9273
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:00:13 +0900

    New translations lang.ot_shipping.php (japanese)

--- admin_languages_packs/japanese/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_shipping.php
+++ admin_languages_packs/japanese/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_shipping.php
@@ -1,11 +1,10 @@
 <?php
-// FIXME - move the last two strings into a shared file in next update
 $define = [
     'MODULE_ORDER_TOTAL_SHIPPING_TITLE' => '配送',
     'MODULE_ORDER_TOTAL_SHIPPING_DESCRIPTION' => '注文合計の送料設定',
     'FREE_SHIPPING_TITLE' => '送料無料',
     'FREE_SHIPPING_DESCRIPTION' => '%s 以上のご購入は送料無料となります。',
-// Beginning of constant configuration titles and descriptions for order total module ot_shipping
+// bof constant configuration titles and descriptions for order total module ot_shipping
     'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_STATUS' => 'このモジュールはインストールされています',
     'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_STATUS' => '',
     'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_SORT_ORDER' => '表示順',
@@ -16,7 +15,7 @@ $define = [
     'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER' => '設定された金額以上の注文には送料を無料にします。',
     'CFGTITLE_MODULE_ORDER_TOTAL_SHIPPING_DESTINATION' => 'この発送元からの注文は送料無料となります',
     'CFGDESC_MODULE_ORDER_TOTAL_SHIPPING_DESTINATION' => '指定された配送先への注文は送料無料の対象となる場合があります。',
-// End of constant configuration titles and descriptions for order total module ot_shipping
+// eof constant configuration titles and descriptions for order total module ot_shipping
 ];
 
 return $define;
