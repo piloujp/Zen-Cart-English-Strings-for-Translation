ff1155fe8d8fac55023866b1084c69b80666f7a5
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 23:00:38 +0900

    New translations lang.system_inspection.php (japanese)

---
+++ admin_languages_packs/japanese/ZenCart_folder/zc_plugins/SystemInspection/1.6.0/admin/includes/languages/english/extra_definitions/lang.system_Inspection.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_SYSTEMINSPECTION' => 'システム検査',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SYSTEMINSPECTION' => '基本 Zen Cart インストールに対して行われたデータベースの変更を一覧表示します。（以前は ModList と呼ばれていました）',
+];
+
+return $define;
