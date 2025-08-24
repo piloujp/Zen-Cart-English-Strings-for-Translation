061c05d4358cd0f374cc899e2cf7d2755d2e5576
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:00:29 +0900

    New translations lang.freecharger.php (french)

--- admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/payment/lang.freecharger.php
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/payment/lang.freecharger.php
@@ -3,7 +3,7 @@ $define = [
     'MODULE_PAYMENT_FREECHARGER_TEXT_TITLE' => 'Charges gratuites',
     'MODULE_PAYMENT_FREECHARGER_TEXT_DESCRIPTION' => 'Utilisée uniquement pour les achats sans frais',
     'MODULE_PAYMENT_FREECHARGER_TEXT_EMAIL_FOOTER' => 'utilisée avec les achats entièrement gratuits',
-// Beginning of constant configuration titles and descriptions for payment module freecharger
+// bof constant configuration titles and descriptions for payment module freecharger
     'CFGTITLE_MODULE_PAYMENT_FREECHARGER_STATUS' => 'Activer le module « Charges gratuites »',
     'CFGDESC_MODULE_PAYMENT_FREECHARGER_STATUS' => 'Souhaitez-vous accepter les paiements « sans frais » ?',
     'CFGTITLE_MODULE_PAYMENT_FREECHARGER_SORT_ORDER' => 'Ordre de tri',
@@ -12,7 +12,7 @@ $define = [
     'CFGDESC_MODULE_PAYMENT_FREECHARGER_ZONE' => 'Si une zone est sélectionnée, activez uniquement ce mode de paiement pour cette zone.',
     'CFGTITLE_MODULE_PAYMENT_FREECHARGER_ORDER_STATUS_ID' => 'Définir le statut de la commande',
     'CFGDESC_MODULE_PAYMENT_FREECHARGER_ORDER_STATUS_ID' => 'Définir le statut des commandes effectuées avec ce module de paiement sur cette valeur :',
-// End of constant configuration titles and descriptions for payment module freecharger
+// eof constant configuration titles and descriptions for payment module freecharger
 ];
 
 return $define;
