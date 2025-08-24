1ac6dd9288c1df6d00c918144f427966b97e5072
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 22:00:32 +0900

    New translations lang.moneyorder.php (japanese)

--- admin_languages_packs/japanese/ZenCart_folder/includes/languages/english/modules/payment/lang.moneyorder.php
+++ admin_languages_packs/japanese/ZenCart_folder/includes/languages/english/modules/payment/lang.moneyorder.php
@@ -2,7 +2,7 @@
 $define = [
     'MODULE_PAYMENT_MONEYORDER_TEXT_TITLE' => '小切手／マネーオーダー',
     'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => '顧客は支払いを郵送することができます。 注文確認メールでは、次のことを求められます。 <br><br>小切手またはマネーオーダーの支払い先は次のとおりです：<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? MODULE_PAYMENT_MONEYORDER_PAYTO : '<br>(あなたの店名)') . '<br><br>お支払いは次の宛先に郵送してください：<br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'ご注文は、支払いを受け取るまで発送されません。',
-// Beginning of constant configuration titles and descriptions for payment module moneyorder
+// bof constant configuration titles and descriptions for payment module moneyorder
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_STATUS' => '小切手／マネーオーダーを有効にする',
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_STATUS' => '小切手／マネーオーダーを受け取りますか？',
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_PAYTO' => '支払先：',
@@ -13,7 +13,7 @@ $define = [
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_ZONE' => '地帯が選択されている場合は、その地帯に対してのみこの支払い方法を有効にしてください。',
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_ORDER_STATUS_ID' => '注文ステータスの設定',
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_ORDER_STATUS_ID' => 'この支払いモジュールで行われた注文のステータスを設定します。',
-// End of constant configuration titles and descriptions for payment module moneyorder
+// eof constant configuration titles and descriptions for payment module moneyorder
 ];
 if (defined('MODULE_PAYMENT_MONEYORDER_STATUS')) {
     $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = '小切手またはマネーオーダーの支払い先は次のとおりです：
