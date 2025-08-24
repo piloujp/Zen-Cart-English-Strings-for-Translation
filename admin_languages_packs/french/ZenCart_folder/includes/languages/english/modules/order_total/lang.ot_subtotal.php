7746abad6ba941a428629b06b1f44179abcd0e04
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 19:06:38 +0900

    New translations lang.ot_subtotal.php (french)

---
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/order_total/lang.ot_subtotal.php
@@ -0,0 +1,13 @@
+<?php
+$define = [
+    'MODULE_ORDER_TOTAL_SUBTOTAL_TITLE' => 'Sous-Total',
+    'MODULE_ORDER_TOTAL_SUBTOTAL_DESCRIPTION' => 'Sous-Total Commande',
+// Beginning of constant configuration titles and descriptions for order total module ot_subtotal
+    'CFGTITLE_MODULE_ORDER_TOTAL_SUBTOTAL_STATUS' => 'Ce module est installé',
+    'CFGDESC_MODULE_ORDER_TOTAL_SUBTOTAL_STATUS' => '',
+    'CFGTITLE_MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER' => 'Ordre d\'affichage.',
+// End of constant configuration titles and descriptions for order total module ot_subtotal
+];
+
+return $define;
