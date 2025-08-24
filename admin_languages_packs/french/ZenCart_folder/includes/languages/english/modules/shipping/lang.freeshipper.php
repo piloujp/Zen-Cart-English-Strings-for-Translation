05338571ea0a2330231b6a9c37f9a21614e59ed4
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 23:00:20 +0900

    New translations lang.freeshipper.php (french)

---
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/shipping/lang.freeshipper.php
@@ -0,0 +1,22 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_FREESHIPPER_TEXT_TITLE' => 'LIVRAISON GRATUITE !',
+    'MODULE_SHIPPING_FREESHIPPER_TEXT_DESCRIPTION' => 'LIVRAISON GRATUITE',
+    'MODULE_SHIPPING_FREESHIPPER_TEXT_WAY' => 'Aucun frais de livraison',
+// bof constant configuration titles and descriptions for freeshipper Shipping
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_STATUS' => 'Activer la livraison gratuite',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_STATUS' => 'Voulez-vous offrir la livraison gratuite ?',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_COST' => 'Frais de livraison',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_COST' => 'Les frais d\'expédition pour toutes les commandes qui utilisent ce mode d\'expédition.',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_HANDLING' => 'Frais de traitement',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_HANDLING' => 'Frais de traitement pour ce mode d\'expédition.',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_TAX_CLASS' => 'Classe de la taxe',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_TAX_CLASS' => 'Utilisez la classe de taxe suivante sur les frais d’expédition.',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_ZONE' => 'Zone d\'expédition',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_ZONE' => 'Si une zone est sélectionnée, activez ce mode d\'expédition uniquement pour cette zone.',
+    'CFGTITLE_MODULE_SHIPPING_FREESHIPPER_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_SHIPPING_FREESHIPPER_SORT_ORDER' => 'Ordre d\'affichage.',
+// eof constant configuration titles and descriptions for freeshipper Shipping
+];
+
+return $define;
