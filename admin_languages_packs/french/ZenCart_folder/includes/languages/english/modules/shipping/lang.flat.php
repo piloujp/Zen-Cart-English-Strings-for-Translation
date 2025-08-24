3f7c8230932a995fb93bded4aa655872204b27d4
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-24 23:00:15 +0900

    New translations lang.flat.php (french)

---
+++ admin_languages_packs/french/ZenCart_folder/includes/languages/english/modules/shipping/lang.flat.php
@@ -0,0 +1,22 @@
+<?php
+$define = [
+    'MODULE_SHIPPING_FLAT_TEXT_TITLE' => 'Au Forfait',
+    'MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION' => 'Forfait de livraison',
+    'MODULE_SHIPPING_FLAT_TEXT_WAY' => 'La meilleure façon',
+// bof constant configuration titles and descriptions for flat Shipping
+    'CFGTITLE_MODULE_SHIPPING_FLAT_STATUS' => 'Activer l\'expédition forfaitaire',
+    'CFGDESC_MODULE_SHIPPING_FLAT_STATUS' => 'Souhaitez-vous proposer des frais de port à tarif forfaitaire ?',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_COST' => 'Frais de livraison',
+    'CFGDESC_MODULE_SHIPPING_FLAT_COST' => 'Les frais d\'expédition pour toutes les commandes qui utilisent ce mode d\'expédition.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Classe de la taxe',
+    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_CLASS' => 'Utilisez la classe de taxe suivante sur les frais d’expédition.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'Base de calcul de la taxe',
+    'CFGDESC_MODULE_SHIPPING_FLAT_TAX_BASIS' => 'Sur quelle base la taxe d\'expédition est-elle calculée ? Les options sont :<br>Expédition - Basée sur l\'adresse d\'expédition du client<br>Facturation - Basée sur l\'adresse de facturation du client<br>Magasin - Basée sur l\'adresse du magasin si la zone de facturation/d\'expédition est égale à la zone du magasin',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_ZONE' => 'Zone d\'expédition',
+    'CFGDESC_MODULE_SHIPPING_FLAT_ZONE' => 'Si une zone est sélectionnée, activez ce mode d\'expédition uniquement pour cette zone.',
+    'CFGTITLE_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Ordre de tri',
+    'CFGDESC_MODULE_SHIPPING_FLAT_SORT_ORDER' => 'Ordre d\'affichage.',
+// eof constant configuration titles and descriptions for flat Shipping
+];
+
+return $define;
