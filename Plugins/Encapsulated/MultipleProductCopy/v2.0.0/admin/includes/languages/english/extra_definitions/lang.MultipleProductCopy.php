4b12f42bd8f89c1ea22863970e4eece6c3749948
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-27 04:54:45 +0900

    Plugins files names update to use their unique key

---
+++ Plugins/Encapsulated/MultipleProductCopy/v2.0.0/admin/includes/languages/english/extra_definitions/lang.MultipleProductCopy.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_MULTIPLEPRODUCTCOPY' => 'Multiple Product Copy',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_MULTIPLEPRODUCTCOPY' => 'Perform bulk product actions for Copy-link, Duplicate, Move, Delete etc...',
+];
+
+return $define;
