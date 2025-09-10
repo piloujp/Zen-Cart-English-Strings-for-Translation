8f8da5806216342fe595ddb01babebf693f4d4dc
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-10 11:35:33 +0900

    Added plugin Monthly Sales and Taxes Report file

---
+++ Plugins/Encapsulated/MonthlySalesTaxReport/v3.0.1/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -0,0 +1,8 @@
+<?php
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_MONTHLYSALESTAXREPORT' => 'Monthly Sales and Tax Summary',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_MONTHLYSALESTAXREPORT' => 'This report displays a summary of monthly or daily totals:<ul><li>gross income (order totals)</li><li>subtotals of all orders in the selected period</li><li>non taxed sales subtotals</li><li>taxed sales subtotals</li><li>tax collected</li><li>shipping/handling charges</li><li>low order fees (if present)</li><li>gift vouchers (or other added order total component, if present)</li></ul><br>The data comes from the orders and orders_total tables.',
+];
+
+return $define;
