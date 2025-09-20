8d40358627d3edc21d828ccc43fc25c3be74dcd3
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-21 03:40:14 +0900

    Updated plugin One Page Checkout version to 2.5.5 - POSM correction - Added Sync additional images

---
+++ ZenCart_folder/zc_plugins/SyncAdditionalImages/v1.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_SYNCADDITIONALIMAGES' => 'Sync Additional Product Images to Database',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SYNCADDITIONALIMAGES' => 'Tool to convert additional product images from filesystem into database, and keep them in sync.',
+];
+
+return $define;
