d6ac1cf23dd138cd60548fd51b4806d023094120
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-22 16:07:19 +0900

    Added Clone Template plugin file

---
+++ Plugins/Encapsulated/CloneTemplate/v2.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_CLONETEMPLATE' => 'Clone a Template',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_CLONETEMPLATE' => '"Clone" an existing template in your store, copying all the "source" template\'s template-override folders/files and its "Layout Boxes Controller" configuration to a new template.',
+];
+
+return $define;
