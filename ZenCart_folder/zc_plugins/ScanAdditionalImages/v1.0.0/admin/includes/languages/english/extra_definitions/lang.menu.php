79308df7caccc1ba2e5dcb569642b514b0b33b35
piloujp <117885587+piloujp@users.noreply.github.com>
2025-09-29 23:10:28 +0900

    Files update for Scan Additional Images feature

--- ZenCart_folder/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
+++ ZenCart_folder/zc_plugins/ScanAdditionalImages/v1.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -2,6 +2,8 @@
 $define = [
     'ADMIN_PLUGIN_MANAGER_NAME_FOR_SCANADDITIONALIMAGES' => 'Scan For Additional Product Image Files for Database',
     'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SCANADDITIONALIMAGES' => 'Tool to link a product with its additional images found on server and matching legacy filename patterns.',
+    'CFGTITLE_ADDITIONAL_IMAGES_MODE' => 'Additional Images filename matching pattern',
+    'CFGDESC_ADDITIONAL_IMAGES_MODE' => 'In Filename-Matching mode, you can use an &quot;_&quot; suffix in two formats:<br>&quot;strict&quot; = always use &quot;_&quot; suffix<br>&quot;legacy&quot; = only use &quot;_&quot; suffix in subdirectories<br>(Before v210 legacy was the default)<br>Default = strict',
 ];
 
 return $define;
