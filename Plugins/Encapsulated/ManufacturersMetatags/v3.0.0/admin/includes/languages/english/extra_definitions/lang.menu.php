cc66bb3ef1721e8806392c7dc5645c28f2c27af8
piloujp <117885587+piloujp@users.noreply.github.com>
2025-10-30 10:52:07 +0900

    Added Manufacturers Metatags plugin's files

---
+++ Plugins/Encapsulated/ManufacturersMetatags/v3.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -0,0 +1,8 @@
+<?php
+
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_MANUFACTURERSMETATAGS' => 'Manufacturers Metatags',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_MANUFACTURERSMETATAGS' => 'This plugin enables your store to assign manufacturer-specific meta-tag title, keyword and/or description text, in a manner similar to the product-specific meta-tag processing that is built into Zen Cart.',
+];
+
+return $define;
