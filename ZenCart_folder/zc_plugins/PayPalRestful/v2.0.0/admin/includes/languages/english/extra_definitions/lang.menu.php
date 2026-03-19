5a0fed1715bd758124d34496978eeca9e9bef831
piloujp <117885587+piloujp@users.noreply.github.com>
2026-03-19 18:31:34 +0900

    PayPal RESTFul as encapsulated by default

---
+++ ZenCart_folder/zc_plugins/PayPalRestful/v2.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_PAYPALRESTFUL' => 'PayPal Restful Payment Module',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_PAYPALRESTFUL' => "This Zen Cart payment module combines the processing for the <b>PayPal Payments Pro</b> (<var>paypaldp</var>) and <b>PayPal Express Checkout</b> (<var>paypalwpp</var>) payment modules that are currently built into the Zen Cart distribution.  Instead of using the older NVP (<b>N</b>ame <b>V</b>alue <b>P</b>air) methods to communicate with PayPal, this payment module uses PayPal's now-current <a href=\"https://developer.paypal.com/api/rest/\" target=\"_blank\">REST APIs</a> and combines the two legacy methods into one.<br><br>Based on the like-named unencapsulated v1.3.1 version of the payment module.",
+];
+
+return $define;
