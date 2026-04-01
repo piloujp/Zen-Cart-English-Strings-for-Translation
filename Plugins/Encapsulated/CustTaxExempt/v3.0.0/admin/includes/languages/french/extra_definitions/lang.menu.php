dd4ac976344faa2530cd6b72c5859dbf549e50c7
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-02 00:57:26 +0900

    Plugin Customer Tax Exempted added

---
+++ Plugins/Encapsulated/CustTaxExempt/v3.0.0/admin/includes/languages/french/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_CUSTTAXEXEMPT' => 'Customer Tax Exempt',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_CUSTTAXEXEMPT' => 'This plugin enables an admin to indicate that a customer is exempt from taxes.',
+];
+
+return $define;
