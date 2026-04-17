bd5c08d95ba6214285eaea272fa892863b8469e5
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-18 02:50:52 +0900

    Plugin Sales report II update to v4.0.3

--- Plugins/Encapsulated/SalesReport/v4.0.2/admin/includes/languages/english/extra_definitions/lang.menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-$define = [
-    'ADMIN_PLUGIN_MANAGER_NAME_FOR_SALESREPORT' => 'Sales Report II',
-    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_SALESREPORT' => 'Generate filtered reports of your site\'s sales.',
-];
-
-return $define;
