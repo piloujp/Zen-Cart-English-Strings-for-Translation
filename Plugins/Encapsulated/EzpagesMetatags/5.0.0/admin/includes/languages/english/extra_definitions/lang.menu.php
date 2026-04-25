5e78a8aea72c6dccdd4ef4102aaf9319f8988a40
piloujp <117885587+piloujp@users.noreply.github.com>
2026-04-26 04:25:09 +0900

    Plugins update and addition
    Access Blocker v2.0.3
    Ez Pages Meta-tags v5.0.0

---
+++ Plugins/Encapsulated/EzpagesMetatags/5.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_EZPAGESMETATAGS' => 'EZ Pages Metatags',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_EZPAGESMETATAGS' => 'Allows input of custom meta-tags for EZ Pages.',
+];
+
+return $define;
