5253bcb7b9cf0d8e4f05234f8ed6ae2e118ef033
piloujp <117885587+piloujp@users.noreply.github.com>
2026-05-29 00:18:24 +0900

    Added Responsive Classic template plugin

---
+++ ZenCart_folder/zc_plugins/ResponsiveClassicPlugin/v1.0.0/admin/includes/languages/english/extra_definitions/lang.menu.php
@@ -0,0 +1,7 @@
+<?php
+$define = [
+    'ADMIN_PLUGIN_MANAGER_NAME_FOR_RESPONSIVECLASSICPLUGIN' => 'Responsive Classic Plugin Template',
+    'ADMIN_PLUGIN_MANAGER_DESCRIPTION_FOR_RESPONSIVECLASSICPLUGIN' => 'Responsive classic as a distinct selectable encapsulated template package.',
+];
+
+return $define;
