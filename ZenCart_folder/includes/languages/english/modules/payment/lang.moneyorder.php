9d9961261c92452d977a7f1271ade58d637283ae
piloujp <117885587+piloujp@users.noreply.github.com>
2026-06-06 04:48:35 +0900

    Update to latest ZC v3.0.0 files (zen_config)

--- ZenCart_folder/includes/languages/english/modules/payment/lang.moneyorder.php
+++ ZenCart_folder/includes/languages/english/modules/payment/lang.moneyorder.php
@@ -1,7 +1,10 @@
 <?php
 $define = [
     'MODULE_PAYMENT_MONEYORDER_TEXT_TITLE' => 'Check/Money Order',
-    'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Customers can mail in their payment. Their order confirmation email will ask them to: <br><br>Please make your check or money order payable to:<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? MODULE_PAYMENT_MONEYORDER_PAYTO : '<br>(your store name)') . '<br><br>Mail your payment to:<br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'Your order will not ship until we receive payment.',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Customers can mail in their payment. Their order confirmation email will ask them to:<br><br>Please make your check or money order payable to:<br><br>' . zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO', '(your store name)') . '<br><br>Mail your payment to:<br>' . nl2br(zen_config('STORE_NAME_ADDRESS'), false) . '<br><br>' . 'Your order will not ship until we receive payment.',
+    'MODULE_PAYMENT_MONEYORDER_REMINDER' => 'Please be sure to write your order number on the front of your check or money order.',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_MISSING_INFO' => '(not configured - needs pay-to)',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER' => 'Please make your check or money order payable to:' . "\n\n" . zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO', '') . "\n\n" . 'Mail your payment to:' . "\n" . zen_config('STORE_NAME_ADDRESS') . "\n\n" . 'Your order will not ship until we receive payment.',
 // bof constant configuration titles and descriptions for payment module moneyorder
     'CFGTITLE_MODULE_PAYMENT_MONEYORDER_STATUS' => 'Enable Check/Money Order Module',
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_STATUS' => 'Do you want to accept Check/Money Order payments?',
@@ -15,8 +18,4 @@ $define = [
     'CFGDESC_MODULE_PAYMENT_MONEYORDER_ORDER_STATUS_ID' => 'Set the status of orders made with this payment module to this value',
 // eof constant configuration titles and descriptions for payment module moneyorder
 ];
-if (defined('MODULE_PAYMENT_MONEYORDER_STATUS')) {
-    $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = 'Please make your check or money order payable to:' . "\n\n" . MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\n" . 'Mail your payment to:' . "\n" . STORE_NAME_ADDRESS . "\n\n" . 'Your order will not ship until we receive payment.';
-}
-
 return $define;
